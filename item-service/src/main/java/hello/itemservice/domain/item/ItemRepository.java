ffc62fbe65c8611aae60e010b60f169e6e2c5bfd
yang <soochang.yang@gmail.com>
2026-06-26 18:21:47 +0900

    item-service toy project init

---
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,43 @@
+package hello.itemservice.domain.item;
+
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();  //static
+    private static long sequence = 0L; //static
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        //ArrayList로 한번더 감싸는 이유는 값이 변경되어도  store에 영향을 미치지 않기 때문
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam){
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+
+}
